0163d540af27927c2426a75cc51eed746c91f6ed
computernerdzach <wizteczach@gmail.com>
2022-05-14 18:13:23 -0700

    hamburger to x and back

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,12 @@
-console.log('42');
+const hamburger = document.querySelector('#btnHamburger');
+
+btnHamburger.addEventListener('click', function(){
+    console.log('click hamburger');
+
+    if(btnHamburger.classList.contains('open')){
+        btnHamburger.classList.remove('open');
+    }
+    else {
+        btnHamburger.classList.add('open');
+    }
+});
